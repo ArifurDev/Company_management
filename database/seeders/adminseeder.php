f5138a0960fa4f74a61383851b2918cac441ff6d
Arifur Rahman Rifat <98427204+Arifurrahmanrifat29112002@users.noreply.github.com>
2023-09-24 12:55:25 +0600

    Update adminseeder.php

--- database/seeders/adminseeder.php
+++ database/seeders/adminseeder.php
@@ -17,7 +17,7 @@ class adminseeder extends Seeder
     {
         User::create([
             'name' => 'admin',
-            'email' => 'thesajspa@gmail.com',
+            'email' => 'admin@gmail.com',
             'role' => 'admin',
             'password' => bcrypt('12345678'),
             'email_verified_at' => now(),
